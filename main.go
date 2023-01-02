2ef333af9f2a43f475baad317458d3fcc53ed1be
Kazuma Watanabe <watassbass@gmail.com>
2023-01-02 17:23:24 +0000

    Initial implementation

--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
-	"github.com/terraform-linters/tflint-ruleset-template/rules"
+	"github.com/terraform-linters/tflint-ruleset-opa/opa"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		RuleSet: &tflint.BuiltinRuleSet{
-			Name:    "template",
-			Version: "0.1.0",
-			Rules: []tflint.Rule{
-				rules.NewAwsInstanceExampleTypeRule(),
-				rules.NewAwsS3BucketExampleLifecycleRule(),
-				rules.NewGoogleComputeSSLPolicyRule(),
-				rules.NewTerraformBackendTypeRule(),
+		RuleSet: &opa.RuleSet{
+			BuiltinRuleSet: tflint.BuiltinRuleSet{
+				Name:    "opa",
+				Version: "0.1.0",
 			},
 		},
 	})
